cbf58ff3f6f822f0bbdf149cb76f719be6944e14
Joan Nyambura <njugunajoan35@gmail.com>
2024-09-13 21:00:18 +0300

    Assignment: Compound Expectations

--- spec/compound_expectations_spec.rb
+++ spec/compound_expectations_spec.rb
@@ -22,3 +22,29 @@ RSpec.describe [:usa, :canada, :mexico] do
     expect(subject.sample).to eq(:usa).or eq(:canada).or eq(:mexico)
   end
 end
+
+# Assignment compound expectations
+
+RSpec.describe 'firetruck' do
+  it 'should start with fire and end with truck' do
+    expect(subject).to start_with('fire').and end_with('truck')
+  end
+
+  it { is_expected.to start_with('fire').and end_with('truck') }
+end
+
+RSpec.describe 20 do
+  it 'should be even and respond to the times method' do
+    expect(subject).to be_even.and respond_to(:times)
+  end
+
+  it { is_expected.to be_even.and respond_to(:times) }
+end
+
+RSpec.describe [4, 8, 15, 16, 23, 42] do
+  it 'should include 42 and start with the values 4, 8, 15' do
+    expect(subject).to include(42).and start_with(4, 8, 15)
+  end
+
+  it { is_expected.to include(42).and start_with(4, 8, 15) }
+end
