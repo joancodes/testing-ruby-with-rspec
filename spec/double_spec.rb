1c9083dfe57485e61cb2f2e628769ec3ad48ba9d
Joan Nyambura <njugunajoan35@gmail.com>
2024-09-16 11:33:24 +0300

    Assignment: Doubles

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -14,3 +14,44 @@ RSpec.describe 'a random double' do
     expect(stuntman.light_on_fire).to eq(true)
   end
 end
+
+# Double: Assignment
+# Create a double with the name "Database Connection".
+
+# The double should have a method called connect that returns the value true.
+
+# The double also have a method called disconnect that returns the value "Goodbye".
+
+# The double's methods should be assigned in the initial invocation of the double method.
+
+# Write two expectations, one for connect and one for disconnect, that confirms the return value of each.
+
+# Assign the double to the variable db.
+
+RSpec.describe 'Database Connection' do
+  it 'connects to a database' do
+    db = double('Database Connection', connect: true, disconnect: 'Goodbye')
+    expect(db.connect).to eq(true)
+    expect(db.disconnect).to eq('Goodbye')
+  end
+
+  it 'disconnects from a database' do
+    db = double('Database Connection', connect: true, disconnect: 'Goodbye')
+    expect(db.disconnect).to eq('Goodbye')
+  end
+end
+
+# Create a double with the name "File System". Assign the double to the variable fs.
+
+# Using the allow method, give the double a read method that returns the value "Romeo and Juliet".
+
+# Using the allow method, give the double a write method that returns the value false.
+
+RSpec.describe 'File System' do
+  it 'reads and writes files' do
+    fs = double('File System')
+    allow(fs).to receive_messages(read: 'Romeo and Juliet', write: false)
+    expect(fs.read).to eq('Romeo and Juliet')
+    expect(fs.write).to eq(false)
+  end
+end
